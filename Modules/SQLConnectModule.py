c68c46ca12fc0b4aaf8dcbd9cd61ca624f80cdac
Jyrs <gj-krsnv@ya.ru>
2022-10-15 23:11:00 +0300

    SQLConnect module with Singleton pattern

---
+++ Modules/SQLConnectModule.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+
+class SQLConnect:
+    __instance = None
+
+    def __init__(self, db_name):
+        self.sqlite_connection = sqlite3.connect(db_name)
+        self.sql_handler = self.sqlite_connection.cursor()
+
+    @classmethod
+    def getConnect(cls, db_name):
+        if not cls.__instance:
+            try:
+                sqlite_connection = sqlite3.connect(db_name)
+            except sqlite3.Error as error:
+                print(error)
+            finally:
+                if sqlite_connection:
+                    cls.__instance = SQLConnect(db_name)
+                    sqlite_connection.close()
+                    return cls.__instance
+                else:
+                    return None
+        else:
+            return cls.__instance
